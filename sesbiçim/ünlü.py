a7f3ae431356c7ae3fa367355d9870fd527e2e79
oktayelb <oktayelb@gmail.com>
2026-06-07 20:07:36 +0300

    seslerin sınıflandırması hakkında bilgi düşüldü

--- sesbiçim/ünlü.py
+++ sesbiçim/ünlü.py
@@ -1,3 +1,5 @@
 # ünlüleri tek tek tanımlamak yerine ünlü harflerin sahip olabildikleri özellikleri tanımlayıp,
 # herhangi bir ünlü harfi de bu özelliklere sahip/nasahip bir vektör olarak tanımlarsak her türlü
 # yeni ünlü sesin harfini burada tanımlamamız gerekmez, model kendisi bu özellikler üzerinden anlar.
+
+# kalın, ince , düz , yuvarlak gibi.
--- sesbiçim/ünsüz.py
+++ sesbiçim/ünsüz.py
@@ -1,3 +1,5 @@
 # ünsüzleri tek tek tanımlamak yerine ünsüz harflerin sahip olabildikleri özellikleri tanımlayıp,
 # herhangi bir ünsüz harfi de bu özelliklere sahip/nasahip bir vektör olarak tanımlarsak her türlü
 # yeni ünsüz sesin harfini burada tanımlamamız gerekmez, model kendisi bu özellikler üzerinden anlar.
+
+# dudaksıllar, patlayıcılar, burunsullar gibi.
